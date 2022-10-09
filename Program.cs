e7be5d6235915a99d07989d12d1abd5399968f9f
Caio Ruan <caioruan36@gmail.com>
2022-10-08 23:20:33 -0400

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using testeClasse;
+
+List<Pessoa> listaDePessoa = new List<Pessoa>();
+
+while (listaDePessoa.Count() < 5)
+{
+    listaDePessoa.Add(CriarPessoa());
+    Console.WriteLine("Pessoa: " + listaDePessoa.Last().nome + " adicionada na lista");
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadKey();
+    Console.Clear();
+
+}
+
+foreach (Pessoa pessoa in listaDePessoa)
+{
+    Console.WriteLine($"Nome: {pessoa.nome} || Idade: {pessoa.idade}");
+}
+Console.ReadLine();
+
+Pessoa CriarPessoa()
+{
+    Console.WriteLine("Insira o nome do usúario: ");
+    string nome = Console.ReadLine();
+    Console.WriteLine("Insira a idade do usúario: ");
+    int idade = int.Parse(Console.ReadLine());
+
+    Pessoa pessoa = new Pessoa()
+    {
+        nome = nome,
+        idade = idade
+    };
+    Console.Clear();
+    return pessoa;
+}
